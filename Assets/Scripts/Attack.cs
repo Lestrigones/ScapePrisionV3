3981bd0f351140d11886929feaba1aa8a43936ac
Lestrigones <40467401+Lestrigones@users.noreply.github.com>
2019-01-19 22:40:52 +0100

    Version 3.2 :)

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+    //Este ataque es para matar al madero
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        ///--- Restamos uno de vida si es un enemigo
+        if (col.tag == "Policia2") col.SendMessage("Attacked");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
